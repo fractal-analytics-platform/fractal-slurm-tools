4a4aba0c18cabaf158bf3be85ac3f8c4c41b5f85
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-07-11 15:31:14 +0200

    Log error

--- src/fractal_slurm_tools/run_sacct_command.py
+++ src/fractal_slurm_tools/run_sacct_command.py
@@ -1,5 +1,6 @@
 import shlex
 import subprocess  # nosec
+import sys
 
 from .sacct_fields import DELIMITER
 from .sacct_fields import SACCT_FMT
@@ -30,6 +31,11 @@ def run_sacct_command(job_string: str) -> str:
         shlex.split(cmd),
         capture_output=True,
         encoding="utf-8",
-        check=True,
+        check=False,
     )
+    if res.returncode != 0:
+        print(f"ERROR in running {cmd=}.")
+        print(f"STDOUT:\n{res.stdout}")
+        print(f"STDERR:\n{res.stderr}")
+        sys.exit("Exit.")
     return res.stdout
