0ea906ff47e4e2abeb75da328a51f02b05306910
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2025-09-16 11:39:08 +0200

    test precommit

--- tests/test_get_job_submit_start_end_times.py
+++ tests/test_get_job_submit_start_end_times.py
@@ -9,10 +9,10 @@ def test_get_job_submit_start_end_times():
         "2025-07-22T08:44:14|2025-07-22T08:44:18|COMPLETED|00:00:04|00:00:04|"
         "4|00:00.140|1||||||||||||||||billing=1,cpu=1,mem=2000M,node=1|"
         "billing=1,cpu=1,mem=2000M,node=1|standard|normal|sbatch --parsable "
-        "/shares/prbvc.biovision.uzh/fractal/bvc/"
+        "/shares/pr.biovision/fractal//"
         "proj_v2_0000213_wf_0000243_job_0000538_20250722_064409/"
         "0___test_echo_task__/non_par-slurm-submit.sh|/shares/"
-        "prbvc.biovision.uzh/fractal/bvc/"
+        "pr.biovision/fractal//"
         "proj_v2_0000213_wf_0000243_job_0000538_20250722_064409/"
         "0___test_echo_task__|4\n"
         "22496092.batch|batch|u20-cva0000-009|1|2025-07-22T08:44:14|"
@@ -27,15 +27,16 @@ def test_get_job_submit_start_end_times():
         "2025-07-22T08:44:15|2025-07-22T08:44:17|COMPLETED|00:00:02|00:00:02|"
         "2|00:00.113|1|0|0|0|0|0|0|0|0|0|266248K|266248K|0|00:00:00|00:00:00|"
         "0||cpu=1,mem=2000M,node=1|||srun --ntasks=1 --cpus-per-task=1 "
-        "--mem=2000MB /shares/prbvc.biovision.uzh/fractal/bvc/env/bin/python "
+        "--mem=2000MB /shares/pr.biovision/fractal//env/bin/python "
         "-m fractal_server.app.runner.executors.slurm_common.remote "
-        "--input-file /shares/prbvc.biovision.uzh/fractal/bvc/"
+        "--input-file /shares/pr.biovision/fractal//"
         "proj_v2_0000213_wf_0000243_job_0000538_20250722_064409/"
         "0___test_echo_task__/non_par--input.json --output-file "
-        "/shares/prbvc.biovision.uzh/fractal/bvc/"
+        "/shares/pr.biovision/fractal//"
         "proj_v2_0000213_wf_0000243_job_0000538_20250722_064409/"
         "0___test_echo_task__/non_par--output.json||2\n"
     )
+
     lines = STDOUT.splitlines()
 
     with pytest.raises(ValueError):
