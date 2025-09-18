bff229a2f47658d0ac115272ad886d4f7cd78b15
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2025-09-18 10:44:29 +0200

    remove Appending

--- src/fractal_slurm_tools/sacct_parser_functions.py
+++ src/fractal_slurm_tools/sacct_parser_functions.py
@@ -14,7 +14,6 @@ def _identity(arg: str) -> str:
 
 def _str_to_float_to_int(arg: str) -> int:
     if arg.strip() == "":
-        logger.error("Appending: Empty string where float expected.")
         ERRORS.append("Empty string where float expected.")
         return 0
     return int(float(arg))
@@ -25,7 +24,6 @@ def _dhhmmss_to_seconds(arg: str) -> int:
     Supports both `HH:MM:SS` and `D-HH:MM:SS`.
     """
     if arg.strip() == "":
-        logger.error("Appending: Empty string where time expected.")
         ERRORS.append("Empty string where time expected.")
         return 0
     if "-" in arg:
@@ -43,7 +41,6 @@ def _str_to_datetime(arg: str) -> str:
 
 def _str_to_bytes(arg: str) -> int:
     if arg.strip() == "":
-        logger.error("Appending: Empty string where string expected.")
         ERRORS.append("Empty string where string expected.")
         return 0
     return humanfriendly.parse_size(arg)
